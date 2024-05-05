494a2718eb72cbb2a940f27c86ab65f8e54ad252
ljh4697 <68049281+ljh4697@users.noreply.github.com>
2024-05-05 14:24:32 +0900

    add argparser -v -m

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import pandas as pd
-from sklearn.metrics import silhouette_samples, silhouette_score
-from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import PCA
 from sklearn.mixture import GaussianMixture
 import seaborn as sns
@@ -10,6 +8,14 @@ from sklearn.cluster import KMeans, DBSCAN, Birch
 from sklearn.preprocessing import StandardScaler
 from preprocess_ import preprocess
 from Modeling import train_models
+import argparse, sys
+
+
+
+Argparser = argparse.ArgumentParser()
+Argparser.add_argument('-v', help=' : set for visualize clustering (1 or 0)',  choices=[0, 1], default=1, type=int)
+Argparser.add_argument('-m', help=' : set for training machielearning model (1 or 0)', choices=[0, 1], default=1, type=int)
+args = Argparser.parse_args()
 
 data_set, selected_for_x, target = preprocess("aiml_test_data.xlsx")
 
@@ -44,45 +50,50 @@ def determine_clusters(data, max_clusters=10):
 
 # 엘보우 메소드 실행
 determine_clusters(PCA_Data)
-
 PCA_Data.plot.scatter(x='pca1', y='pca2')
 
-'''clustering 시각화'''
-# KMeans
-kmeans_4 = KMeans(n_init=20, n_clusters=5, init='k-means++', random_state=42)
-kmeans_4.fit(PCA_Data)
-PCA_Data = pd.concat([PCA_Data ,pd.DataFrame(kmeans_4.predict(PCA_Data), columns=['KmeansC'])], axis=1)
-ax = sns.scatterplot(x='pca1', y='pca2', hue='KmeansC', data=PCA_Data, palette="deep")
-sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
-plt.title('Kmeans')
-plt.show()
-# GMM
-gmm = GaussianMixture(n_components=5, random_state=42)
-gmm.fit(PCA_Data[['pca1', 'pca2']].copy())
-PCA_Data['gmmC'] = gmm.predict(PCA_Data[['pca1', 'pca2']].copy())
-ax = sns.scatterplot(x='pca1', y='pca2', hue='gmmC', data=PCA_Data, palette="deep")
-sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
-plt.title('GMM')
-plt.show()
-# DBSCAN
-DBSCAN_model = DBSCAN(eps=0.087, min_samples=8)
-DBSCAN_model.fit((PCA_Data[['pca1', 'pca2']].copy()))
-PCA_Data['DBSCAN'] = DBSCAN_model.fit_predict(PCA_Data[['pca1', 'pca2']].copy())
-ax = sns.scatterplot(x='pca1', y='pca2', hue='DBSCAN', data=PCA_Data, palette="deep")
-sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
-plt.title('DBSCAN')
-plt.show()
-# BIRCH
-birchmodel = Birch(n_clusters=5)
-birchmodel.fit((PCA_Data[['pca1', 'pca2']].copy()))
-PCA_Data['birchC'] = birchmodel.fit_predict(PCA_Data[['pca1', 'pca2']].copy())
-ax = sns.scatterplot(x='pca1', y='pca2', hue='birchC', data=PCA_Data, palette="deep")
-sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
-plt.title('BIRCH')
-plt.show()
+
+if args.v:
+    #clustering 시각화
+    # KMeans
+    kmeans_4 = KMeans(n_init=20, n_clusters=5, init='k-means++', random_state=42)
+    kmeans_4.fit(PCA_Data)
+    PCA_Data = pd.concat([PCA_Data ,pd.DataFrame(kmeans_4.predict(PCA_Data), columns=['KmeansC'])], axis=1)
+    ax = sns.scatterplot(x='pca1', y='pca2', hue='KmeansC', data=PCA_Data, palette="deep")
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    plt.title('Kmeans')
+    plt.show()
+    # GMM
+    gmm = GaussianMixture(n_components=5, random_state=42)
+    gmm.fit(PCA_Data[['pca1', 'pca2']].copy())
+    PCA_Data['gmmC'] = gmm.predict(PCA_Data[['pca1', 'pca2']].copy())
+    ax = sns.scatterplot(x='pca1', y='pca2', hue='gmmC', data=PCA_Data, palette="deep")
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    plt.title('GMM')
+    plt.show()
+    # DBSCAN
+    DBSCAN_model = DBSCAN(eps=0.087, min_samples=8)
+    DBSCAN_model.fit((PCA_Data[['pca1', 'pca2']].copy()))
+    PCA_Data['DBSCAN'] = DBSCAN_model.fit_predict(PCA_Data[['pca1', 'pca2']].copy())
+    ax = sns.scatterplot(x='pca1', y='pca2', hue='DBSCAN', data=PCA_Data, palette="deep")
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    plt.title('DBSCAN')
+    plt.show()
+    # BIRCH
+    birchmodel = Birch(n_clusters=5)
+    birchmodel.fit((PCA_Data[['pca1', 'pca2']].copy()))
+    PCA_Data['birchC'] = birchmodel.fit_predict(PCA_Data[['pca1', 'pca2']].copy())
+    ax = sns.scatterplot(x='pca1', y='pca2', hue='birchC', data=PCA_Data, palette="deep")
+    sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
+    plt.title('BIRCH')
+    plt.show()
+
+else:
+    birchmodel = Birch(n_clusters=5)
+    birchmodel.fit((PCA_Data[['pca1', 'pca2']].copy()))
+    PCA_Data['birchC'] = birchmodel.fit_predict(PCA_Data[['pca1', 'pca2']].copy())
 
 X['cluster'] = PCA_Data['birchC']
-
 total_data = pd.concat([X, Y], axis=1)
 
 # BIRCH cluster 3, 4 분석
@@ -109,4 +120,5 @@ test_data = total_data[(total_data['year']==2024) & (total_data['month']==4)]
 train_data_x, train_data_y = train_data[selected_for_x].copy(), train_data[target].copy()
 test_data_x, test_data_y = test_data[selected_for_x].copy(), test_data[target].copy()
 
-train_models(train_data_x, train_data_y, test_data_x, test_data_y)
+if args.m:
+    train_models(train_data_x, train_data_y, test_data_x, test_data_y)
